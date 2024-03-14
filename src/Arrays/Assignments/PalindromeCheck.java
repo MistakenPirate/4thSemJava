05389633b09c3f6435cec1af903e9836e03b5b9e
MistakenPirate <mistakenpirate38@gmail.com>
2024-03-14 22:44:10 +0530

    Assignments added

---
+++ src/Arrays/Assignments/PalindromeCheck.java
@@ -0,0 +1,24 @@
+package Arrays.Assignments;
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a palindrome");
+    }
+}
---
+++ src/Arrays/Assignments/RemoveCharacter.java
@@ -0,0 +1,4 @@
+package Arrays.Assignments;
+
+public class RemoveCharacter {
+}
